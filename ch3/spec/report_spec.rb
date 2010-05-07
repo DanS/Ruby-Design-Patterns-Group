3a2bca7f85d7eff0b2701ea2c55ea6bde521fdb9
Dan Steinicke <dansteinicke@gmail.com>
2010-05-07 11:09:25 -0700

    Eliminate significance of whitespace in the expectations by using regex.

--- ch3/spec/report_spec.rb
+++ ch3/spec/report_spec.rb
@@ -3,8 +3,8 @@ require "report.rb"
 describe "report" do
   before(:each) do
     @title = 'Monthly Report'
-    @text =  ['Things are going', 'really, really well.']
-    @tags = ["<html>", "</html>", "  <head>", "  </head>", "  <body>","  </body>"]
+    @text = ['Things are going', 'really, really well.']
+    @tags = ["<html>", "<\/html>", "  <head>", "<\/head>", "<body>","<\/body>"]
     @messenger = mock("messenger").as_null_object
     @report = Report.new(@messenger)
   end
@@ -12,17 +12,17 @@ describe "report" do
   context "should output a html formatted report when passed :html argument" do
     it "should output html tags" do
       for tag in @tags
-        @messenger.should_receive(:puts).with(tag)
+        @messenger.should_receive(:puts).with(/#{tag}/)
       end
       @report.output_report(:html)
     end
     it "should have a title tag" do
-      @messenger.should_receive(:puts).with("    <title>Monthly Report</title>")
+      @messenger.should_receive(:puts).with(/<title>Monthly Report<\/title>/)
       @report.output_report(:html)
     end
     it "should have the body text inside p tags" do
       for text in @text
-        @messenger.should_receive(:puts).with("    <p>#{text}</p>")
+        @messenger.should_receive(:puts).with(/<p>#{text}<\/p>/)
       end
       @report.output_report(:html)
     end
@@ -30,8 +30,8 @@ describe "report" do
   
   context "should output plain text when passed :plain argument" do
     it "should not output any html tags" do
-      for tag in @tags + ['<p>', '</p>', '<title>', '</title>']  
-        @messenger.should_not_receive(:puts).with(tag) 
+      for tag in @tags + ['<p>', '<\/p>', '<title>', '<\/title>']  
+        @messenger.should_not_receive(:puts).with(/#{tag}/) 
       end
       @report.output_report(:plain)
     end
@@ -41,7 +41,7 @@ describe "report" do
     end 
     it "should have the body text" do
       for text in @text
-        @messenger.should_receive(:puts).with(text)
+        @messenger.should_receive(:puts).with(/#{text}/)
       end
       @report.output_report(:plain)
     end
